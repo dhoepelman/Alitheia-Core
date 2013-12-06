56143d1ca978016f17f96ca64921cefca92349e6
Rommelzuiger <rommelzuiger@gmail.com>
2013-12-06 17:15:32 +0100

    Added stubs of the tests for projects and misc

---
+++ alitheia/core/src/test/java/eu/sqooss/test/service/webadmin/projects/AddProjectTest.java
@@ -0,0 +1,25 @@
+package eu.sqooss.test.service.webadmin.projects;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class AddProjectTest {
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+    
+    @Test
+    public void AddProjectTest() {
+        //TODO: Test the adding of a project by its project.properties file
+        //TODO: Test the adding of a project by manually entered properties 
+        fail();
+    }
+}
