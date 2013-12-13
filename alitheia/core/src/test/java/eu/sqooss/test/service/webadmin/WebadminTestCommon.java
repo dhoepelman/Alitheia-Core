cad6161f964d41b8aee1465fc44370b400a301c3
David Hoepelman <dhoepelman@gmail.com>
2013-12-13 15:42:00 +0100

    Changed Mockito into JMockit

--- alitheia/core/src/test/java/eu/sqooss/test/service/webadmin/WebadminTestCommon.java
+++ alitheia/core/src/test/java/eu/sqooss/test/service/webadmin/WebadminTestCommon.java
@@ -1,5 +1,9 @@
 package eu.sqooss.test.service.webadmin;
 
+import java.util.Hashtable;
+
+import javax.servlet.http.HttpServletResponse;
+
 import eu.sqooss.impl.service.webadmin.AdminServlet;
 import eu.sqooss.impl.service.webadmin.WebadminServiceImpl;
 
@@ -10,5 +14,24 @@ public class WebadminTestCommon {
     
     public static void setUp() {
         impl = new WebadminServiceImpl();
+    }
+    
+    
+    protected HttpServletResponse doFakeGetRequest(String url, Hashtable<String, String> parameters) {
+    	// TODO: implement fake get request
+    	return null;
+    }
+    
+    protected HttpServletResponse doFakePostRequest(String url, Hashtable<String, String> parameters) {
+    	// TODO: implement fakse post request
+    	return null;
+    }
+    
+    /**
+     * True iff a certain string is in the given response
+     */
+    protected boolean isInOutput(HttpServletResponse hsr) {
+    	// TODO: Implement
+    	return false;
     }
 }
