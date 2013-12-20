451c5cf56dfe382b8c0f727296af395f33a1f443
Rommelzuiger <rommelzuiger@gmail.com>
2013-12-20 16:38:05 +0100

    Added method which we should implement in the re-engineered webadmin

--- alitheia/core/src/main/java/eu/sqooss/impl/service/webadmin/refactored/servlets/ProjectsServlet.java
+++ alitheia/core/src/main/java/eu/sqooss/impl/service/webadmin/refactored/servlets/ProjectsServlet.java
@@ -1,6 +1,8 @@
 package eu.sqooss.impl.service.webadmin.refactored.servlets;
 
 import eu.sqooss.service.cluster.ClusterNodeService;
+import eu.sqooss.service.db.Plugin;
+import eu.sqooss.service.db.StoredProject;
 import eu.sqooss.service.metricactivator.MetricActivator;
 import eu.sqooss.service.scheduler.Scheduler;
 import eu.sqooss.service.updater.UpdaterService;
@@ -10,4 +12,29 @@ public class ProjectsServlet extends AbstractWebadminServlet {
 	private Scheduler sobjSched;
 	private UpdaterService sobjUpdater;
 	private ClusterNodeService sobjClusterNode;
+	
+	
+	private void addProject(StoredProject project) {
+	    // TODO: Add project
+	}
+	
+	private void removeProject(StoredProject project) {
+	    // TODO: Remove project
+	}	
+	
+	private void triggerUpdate(StoredProject project, String updater) {
+	    // TODO: Trigger a specific updater on a project
+	}
+	
+	private void triggerAllUpdate(StoredProject project) {
+	    // TODO: Trigger all updates on a project
+	} 
+	
+	private void triggerAllUpdateNode() {
+	    // TODO: Trigger all updates on all projects	    
+	}
+	
+	private void synchPlugin(StoredProject project, String plugin) {
+	    // TODO: Synch a plugin of a project
+	}
 }
--- alitheia/core/src/main/java/eu/sqooss/impl/service/webadmin/refactored/servlets/StatusServlet.java
+++ alitheia/core/src/main/java/eu/sqooss/impl/service/webadmin/refactored/servlets/StatusServlet.java
@@ -1,7 +1,36 @@
 package eu.sqooss.impl.service.webadmin.refactored.servlets;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+
+import eu.sqooss.service.scheduler.Job;
 import eu.sqooss.service.scheduler.Scheduler;
 
 public class StatusServlet extends AbstractWebadminServlet {
 	private Scheduler sobjSched;
+	
+	private int getJobFailStats() {
+	    // TODO: Return the number of failed jobs
+	    return -1;
+	}
+	
+	
+	private int getJobWaitStats() {
+	 // TODO: Return the number of waiting jobs
+	    return -1;
+	}
+	
+	
+	private int getJobRunStats() {
+	 // TODO: Return the number of running jobs
+	    return -1;
+	}
+	
+	
+	private Job[] getFailedJobs() {
+	 // TODO: Return the failed jobs
+	    return null;
+	}
 }
