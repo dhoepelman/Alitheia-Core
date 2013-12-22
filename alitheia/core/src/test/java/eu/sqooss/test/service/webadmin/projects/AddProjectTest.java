2538a3e35a5845151bb5c101bad2a40feb4b1a4e
David Hoepelman <dhoepelman@gmail.com>
2013-12-22 21:16:06 +0100

    Start of AddProjectTest.java

--- alitheia/core/src/test/java/eu/sqooss/test/service/webadmin/projects/AddProjectTest.java
+++ alitheia/core/src/test/java/eu/sqooss/test/service/webadmin/projects/AddProjectTest.java
@@ -2,17 +2,28 @@ package eu.sqooss.test.service.webadmin.projects;
 
 import static org.junit.Assert.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.velocity.VelocityContext;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.core.modules.junit4.PowerMockRunner;
+import org.powermock.modules.junit4.PowerMockRunner;
+import static org.powermock.api.mockito.PowerMockito.*;
 
 import eu.sqooss.core.AlitheiaCore;
+import eu.sqooss.impl.service.webadmin.AbstractView;
+import eu.sqooss.service.admin.AdminAction;
+import eu.sqooss.service.admin.AdminService;
+import eu.sqooss.service.admin.actions.AddProject;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest(AlitheiaCore.class)
+@PrepareForTest({AlitheiaCore.class})
 public class AddProjectTest {
 
     @BeforeClass
@@ -24,9 +35,33 @@ public class AddProjectTest {
     }
     
     @Test
-    public void AddProjectTest() {
-        //TODO: Test the adding of a project by its project.properties file
+    public void AddProjectTestManual() {
         //TODO: Test the adding of a project by manually entered properties 
+    	// Mock dependencies
+    	PowerMockito.mockStatic(AlitheiaCore.class);
+    	PowerMockito.mockStatic(AbstractView.class);
+    	AdminService mockAS = mock(AdminService.class);
+    	AdminAction mockAA = mock(AdminAction.class);
+    	VelocityContext mockVC = mock(VelocityContext.class);
+    	when(AlitheiaCore.getInstance().getAdminService()).thenReturn(mockAS);
+    	when(mockAS.create(AddProject.MNEMONIC)).thenReturn(mockAA);
+    	
+    	// Test data
+    	Map<String,String> data = new HashMap<String,String>();
+    	data.put("scm", "svn-file:///svn");
+    	data.put("name", "Test");
+    	data.put("bts", "bugzilla-xml:///bugzilla-xml");
+    	data.put("mail", "maildir:///mail");
+    	data.put("web", "http://example.org");
+    	
+    	
+    	// Verification
         fail();
+    }
+    
+    @Test
+    public void AddProjectTestPropertiesFile() {
+    	//TODO: Test the adding of a project by its project.properties file
+    	fail();
     }
 }
