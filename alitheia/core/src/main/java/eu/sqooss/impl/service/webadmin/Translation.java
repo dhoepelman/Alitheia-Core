62e2fece25b50f485cf87818f13cb7cc5c89aa1b
David Hoepelman <dhoepelman@gmail.com>
2014-01-13 11:46:19 +0100

    Fixed translation

--- alitheia/core/src/main/java/eu/sqooss/impl/service/webadmin/Translation.java
+++ alitheia/core/src/main/java/eu/sqooss/impl/service/webadmin/Translation.java
@@ -14,9 +14,9 @@ public enum Translation implements ITranslation {
 
 	// Names of the various resource files
 	private static final String RES_DIR			  = "/translation/webadmin/";
-	private static final String RES_LABELS_FILE   = RES_DIR+"ResourceLabels";
-	private static final String RES_ERRORS_FILE   = RES_DIR+"ResourceErrors";
-	private static final String RES_MESSAGES_FILE = RES_DIR+"ResourceMessages";
+	private static final String RES_LABELS_FILE   = RES_DIR+"labels";
+	private static final String RES_ERRORS_FILE   = RES_DIR+"errors";
+	private static final String RES_MESSAGES_FILE = RES_DIR+"messages";
 
 	// Resource bundles
 	private ResourceBundle resLbl;
@@ -25,22 +25,36 @@ public enum Translation implements ITranslation {
 
 	private Locale locale;
 
+	private Logger logger;
+
 	Translation(Locale locale) {
 		this.locale = locale;
+
+		try{
+			logger = AlitheiaCore.getInstance().getLogManager().createLogger(Logger.NAME_SQOOSS_WEBADMIN);
+		}
+		catch(Exception e2){
+			// We can't even get a logger...
+			// Silently fail, errors will become apparent because of missing translations
+		}
+
 		try {
 			resLbl = ResourceBundle.getBundle(RES_LABELS_FILE,locale);
+		}
+		catch(MissingResourceException e) {
+			if(logger != null) { logger.warn("Missing resource " + RES_LABELS_FILE + " for locale " + locale); }
+		}
+		try {
 			resMsg = ResourceBundle.getBundle(RES_MESSAGES_FILE,locale);
+		}
+		catch(MissingResourceException e) {
+			if(logger != null) { logger.warn("Missing resource " + RES_MESSAGES_FILE + " for locale " + locale); }
+		}
+		try {
 			resErr = ResourceBundle.getBundle(RES_ERRORS_FILE,locale);
 		}
 		catch(MissingResourceException e) {
-			try{
-				AlitheiaCore.getInstance().getLogManager().createLogger(Logger.NAME_SQOOSS_WEBADMIN)
-				.error("Missing resource file(s) for locale " + locale.getLanguage(), e);
-			}
-			catch(Exception e2){
-				// We can't even get a logger...
-				// Silently fail, the error will become apparent because of missing translations
-			}
+			if(logger != null) { logger.warn("Missing resource " + RES_MESSAGES_FILE + " for locale " + locale); }
 		}
 	}
 
