8d4f53c03347c6b0684d79e3582f8df82c3883a1
David Hoepelman <dhoepelman@gmail.com>
2014-01-14 16:40:10 +0100

    Added plugin uninstall and synchronize tests

--- alitheia/core/src/test/java/eu/sqooss/test/service/webadmin/plugins/PluginTest.java
+++ alitheia/core/src/test/java/eu/sqooss/test/service/webadmin/plugins/PluginTest.java
@@ -1,12 +1,18 @@
 package eu.sqooss.test.service.webadmin.plugins;
 
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.*;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 
 import org.junit.*;
 import org.junit.runner.RunWith;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 import eu.sqooss.impl.service.webadmin.servlets.PluginsServlet;
+import eu.sqooss.service.abstractmetric.AlitheiaPlugin;
 import eu.sqooss.service.pa.PluginInfo;
 import eu.sqooss.test.service.webadmin.AbstractWebadminServletTest;
 
@@ -31,11 +37,13 @@ public class PluginTest extends AbstractWebadminServletTest {
 	public void testPluginInstall() throws Exception {
 		final String hash = "PLUGINHASH";
 
+		// Make the mock request
 		when(mockReq.getRequestURI()).thenReturn("/plugins/plugin/action");
 		when(mockReq.getMethod()).thenReturn("POST");
 		when(mockReq.getParameter("hash")).thenReturn(hash);
 		when(mockReq.getParameter("action")).thenReturn("install");
 
+		// Create the mock plugin
 		PluginInfo p = mock(PluginInfo.class);
 		when(p.isInstalled()).thenReturn(false);
 		when(p.getHashcode()).thenReturn(hash);
@@ -45,5 +53,51 @@ public class PluginTest extends AbstractWebadminServletTest {
 
 		// Verify that the plugin has been installed
 		verify(mockPA).installPlugin(hash);
+	}
+
+	@Test
+	public void testPluginUninstall() throws ServletException, IOException {
+		final String hash = "PLUGINHASH";
+
+		// Make the mock request
+		when(mockReq.getRequestURI()).thenReturn("/plugins/plugin/action");
+		when(mockReq.getMethod()).thenReturn("POST");
+		when(mockReq.getParameter("hash")).thenReturn(hash);
+		when(mockReq.getParameter("action")).thenReturn("uninstall");
+
+		// Create the mock plugin
+		PluginInfo p = mock(PluginInfo.class);
+		when(p.isInstalled()).thenReturn(true);
+		when(p.getHashcode()).thenReturn(hash);
+		when(mockPA.getPluginInfo(hash)).thenReturn(p);
+
+		testee.service(mockReq, mockResp);
+
+		// Verify that the plugin has been uninstalled
+		verify(mockPA).uninstallPlugin(hash);
+	}
+
+	@Test
+	public void testPluginSynchronize() throws Exception {
+		final String hash = "PLUGINHASH";
+
+		// Make the mock request
+		when(mockReq.getRequestURI()).thenReturn("/plugins/plugin/action");
+		when(mockReq.getMethod()).thenReturn("POST");
+		when(mockReq.getParameter("hash")).thenReturn(hash);
+		when(mockReq.getParameter("action")).thenReturn("synchronize");
+
+		// Create the mock plugin
+		PluginInfo p = mock(PluginInfo.class);
+		when(p.isInstalled()).thenReturn(true);
+		when(p.getHashcode()).thenReturn(hash);
+		when(mockPA.getPluginInfo(hash)).thenReturn(p);
+		AlitheiaPlugin p2 = mock(AlitheiaPlugin.class);
+		when(mockPA.getPlugin(p)).thenReturn(p2);
+
+		testee.service(mockReq, mockResp);
+
+		// Verify that the plugin has been synchronized
+		verify(mockMA).syncMetrics(eq(p2));
 	}
 }
