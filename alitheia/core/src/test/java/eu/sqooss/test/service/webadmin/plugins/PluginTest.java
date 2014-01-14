c2c13f0fd22ac76344123cc9cfc1021f6f3da2ee
David Hoepelman <dhoepelman@gmail.com>
2014-01-14 16:13:33 +0100

    Created PluginTest with install plugin test

--- alitheia/core/src/test/java/eu/sqooss/test/service/webadmin/plugins/PluginTest.java
+++ alitheia/core/src/test/java/eu/sqooss/test/service/webadmin/plugins/PluginTest.java
@@ -3,10 +3,15 @@ package eu.sqooss.test.service.webadmin.plugins;
 import static org.mockito.Mockito.*;
 
 import org.junit.*;
+import org.junit.runner.RunWith;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import eu.sqooss.impl.service.webadmin.servlets.PluginsServlet;
+import eu.sqooss.service.pa.PluginInfo;
 import eu.sqooss.test.service.webadmin.AbstractWebadminServletTest;
 
+
+@RunWith(PowerMockRunner.class)
 public class PluginTest extends AbstractWebadminServletTest {
 
 	private PluginsServlet testee;
@@ -24,14 +29,21 @@ public class PluginTest extends AbstractWebadminServletTest {
 
 	@Test
 	public void testPluginInstall() throws Exception {
+		final String hash = "PLUGINHASH";
+
 		when(mockReq.getRequestURI()).thenReturn("/plugins/plugin/action");
 		when(mockReq.getMethod()).thenReturn("POST");
-		when(mockReq.getParameter("hash")).thenReturn("PLUGINHASH");
+		when(mockReq.getParameter("hash")).thenReturn(hash);
 		when(mockReq.getParameter("action")).thenReturn("install");
+
+		PluginInfo p = mock(PluginInfo.class);
+		when(p.isInstalled()).thenReturn(false);
+		when(p.getHashcode()).thenReturn(hash);
+		when(mockPA.getPluginInfo(hash)).thenReturn(p);
 
 		testee.service(mockReq, mockResp);
 
 		// Verify that the plugin has been installed
-		verify(mockPA.installPlugin("PLUGINHASH"));
+		verify(mockPA).installPlugin(hash);
 	}
 }
