6636685c35a92f6a39e6b3cf6960a10be4593dc1
David Hoepelman <dhoepelman@gmail.com>
2014-01-15 19:00:33 +0100

    More comments for StaticResourceServlet

--- alitheia/core/src/main/java/eu/sqooss/impl/service/webadmin/servlets/StaticResourceServlet.java
+++ alitheia/core/src/main/java/eu/sqooss/impl/service/webadmin/servlets/StaticResourceServlet.java
@@ -19,8 +19,16 @@ import com.google.common.collect.ImmutableMap;
 public class StaticResourceServlet extends HttpServlet {
 
 	private final String default_page;
+	/**
+	 * The root location inside the resource folder of all static content
+	 */
 	private static final String resourceLocation = "/webadmin/statics";
 
+	/**
+	 * A map of the static content
+	 * Key: the URL as seen by the client (e.g /stylesheet.css)
+	 * Value: tuple (path, mimetype) where path is the path inside the resources folder and mimetype is the MIME type of the file
+	 */
 	private final Map<String, ResourceFile> staticContentMap =
 			new ImmutableMap.Builder<String, ResourceFile>()
 			.put("/screen.css", new ResourceFile(resourceLocation + "/screen.css", "text/css"))
@@ -28,7 +36,7 @@ public class StaticResourceServlet extends HttpServlet {
 			.put("/sqo-oss.png", new ResourceFile(resourceLocation + "/sqo-oss.png", "image/png"))
 			.put("/queue.png", new ResourceFile(resourceLocation + "/queue.png", "image/png"))
 			.put("/uptime.png", new ResourceFile(resourceLocation + "/uptime.png", "image/png"))
-			.put("/greyBack.jpg", new ResourceFile(resourceLocation + "/greyBack.jpg", "image/x-jpg"))
+			.put("/greyBack.jpg", new ResourceFile(resourceLocation + "/greyBack.jpg", "image/jpeg"))
 			.put("/projects.png", new ResourceFile(resourceLocation + "/projects.png", "image/png"))
 			.put("/logs.png", new ResourceFile(resourceLocation + "/logs.png", "image/png"))
 			.put("/metrics.png", new ResourceFile(resourceLocation + "/metrics.png", "image/png"))
@@ -40,10 +48,13 @@ public class StaticResourceServlet extends HttpServlet {
 			.put("/rules.png", new ResourceFile(resourceLocation + "/rules.png", "image/png"))
 			.build();
 
+	/**
+	 * Create a static resource servlet
+	 * @param default_page the default page, the user is forwarder to this url when "/" is accessed
+	 */
 	public StaticResourceServlet(String default_page) {
 		this.default_page = default_page;
 	}
-
 
 
 	@Override
@@ -97,6 +108,9 @@ public class StaticResourceServlet extends HttpServlet {
 		return staticContentMap.keySet();
 	}
 
+	/**
+	 * ResourceFile is a combination of the file path and its mimetype
+	 */
 	private class ResourceFile {
 		public final String filepath;
 		public final String mimetype;
